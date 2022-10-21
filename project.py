4c65a2e7a1f47a7f4ece9f3f5e15e39d72566ca5
mpanlee3 <mpanlee3@gatech.edu>
2022-10-21 16:35:19 -0700

    add price column

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ from flask import send_from_directory
 import dbaccess
 import csv
 import json
+import random
 
 app = Flask(__name__)
 
@@ -15,7 +16,7 @@ def default():
 
 @app.route("/gethotel")
 def gethotel():
-    headings = ["id", "address", "lat", "lon", "name", "numberOfGuests", "roomType", "stars", "url"]
+    headings = ["id", "address", "lat", "lon", "name", "numberOfGuests", "roomType", "stars", "url", "price"]
     result = None
     try:
         db = dbaccess.dataproc()
@@ -34,7 +35,7 @@ def reload():
         db.create_connection("westunion.db")
         db.execute_query("drop table if exists airbnb;")
         db.execute_query("drop table if exists resort;")
-        db.execute_query("create table airbnb (id integer primary key autoincrement, address varchar(256), lat float, lon float, name varchar(64), numberOfGuests int, roomType varchar(16), stars float, url varchar(256));")
+        db.execute_query("create table airbnb (id integer primary key autoincrement, address varchar(256), lat float, lon float, name varchar(64), numberOfGuests int, roomType varchar(16), stars float, url varchar(256), price float);")
 
         with open("data/airbnb.csv", encoding = "utf8") as csvfile:
             csvread = csv.reader(csvfile, delimiter=",")
@@ -46,7 +47,8 @@ def reload():
                             row[i] = 0
                         if i in {0, 3, 230, 232}:
                             row[i] = row[i].replace("'", "")
-                    sql = "insert into airbnb (address, lat, lon, name, numberOfGuests, roomType, stars, url) values ('{}',{},{},'{}',{},'{}',{},'{}');".format(row[0], row[1], row[2], row[3], row[4], row[230], row[231], row[232])
+                    price = round(random.random() * 800 + 200, 2)
+                    sql = "insert into airbnb (address, lat, lon, name, numberOfGuests, roomType, stars, url, price) values ('{}',{},{},'{}',{},'{}',{},'{}', {});".format(row[0], row[1], row[2], row[3], row[4], row[230], row[231], row[232], price)
                     db.execute_query(sql)
                 lcount += 1
 
