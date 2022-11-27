1f05013c4db12c4d4d3971c5a8c6943e8835931e
kcai93 <kellykxcai@gmail.com>
2022-11-27 10:14:41 -0800

    Added fail proof for api connection

--- logic/resort_recommender.py
+++ logic/resort_recommender.py
@@ -11,8 +11,9 @@ import requests
 import json
 import os
 
+# The calculate_similarity function will calculate how similar any resort is given one resort. 1 being the most similar.
 def calculate_similarity(resort,df):
-  print('User chose '+resort+'calculating similarity')
+  #print('User chose '+resort+'calculating similarity')
   # Select resort info (row) based on user selected resort
   row = df.loc[df['resort_name'] == resort]
   #print(row)
@@ -22,10 +23,6 @@ def calculate_similarity(resort,df):
   dataset1 = row.iloc[:, firstcol:lastcol+1].values.tolist()[0]
   #print(dataset1)
   rownum = df.index[df.resort_name == resort][0]
-  #print(rownum)
-  #print(df)
-  #df['similarity']=0
-  #newdf = df
   for i in range(len(df)):
     each = df.loc[i].values.tolist()
     #print(each)
@@ -37,8 +34,9 @@ def calculate_similarity(resort,df):
     df.loc[i, 'similarity']= round(result,2)
   return df
 
+# The calculate_neighbors function will calculate the number of resorts within 50 miles of the given resort.
 def calculate_neighbors(resort,df):
-  print('User chose '+resort+'calculating number of neighbors')
+  #print('User chose '+resort+'calculating number of neighbors')
   df['neighbor_num'] = 0
   # Select resort info (row) based on user selected resort
   row = df.loc[df['resort_name'] == resort]
@@ -46,7 +44,7 @@ def calculate_neighbors(resort,df):
   loncol = df.columns.get_loc("lon")
   #print(loncol)
   resort1loc = tuple(row.iloc[:, latcol:loncol+1].values.tolist()[0])
-  print(resort1loc)
+  #print(resort1loc)
   if math.isnan(resort1loc[0]) or math.isnan(resort1loc[1]):
     pass
   for i in range(len(df)):
@@ -61,41 +59,40 @@ def calculate_neighbors(resort,df):
   newdf = df
   return newdf
 
-#def resort_recommender(difficulty, goal, fav_resort):
-#    conn = sqlite3.connect('westunion.db')
-#    curr = conn.execute(
-#        "select * from resorts limit 5")
-#    resorts = curr.fetchall()
-#    column_names = ['index', 'Resort', 'State', 'Summit', 'Base', 'Vertical',
-#                                                                           'Lifts', 'Runs', 'Acres', 'Green Percent', 'Green Acres',
-#                                                                           'Blue Percent', 'Blue Acres', 'Black Percent', 'Black Acres',
-#                                                                           'Lat', 'Lon']
-#    resorts_df = pd.DataFrame(resorts, columns = column_names)
-#    resorts_df.drop('index', axis=1, inplace=True)
-#    return resorts_df
+# The get_weather function will use weather api to first get the grid info then weather forecast info for the next 12 hrs based on the lat and lon of the resort.
 def get_weather(lat,lon):
   if math.isnan(lat) or math.isnan(lon):
     pass
   else:
-    add = '/'+str(lat)+','+str(lon)
-    response_API = requests.get('https://api.weather.gov/points'+add)
-    data = response_API.text
-    parse_json = json.loads(data)
-    gridId = str(parse_json['properties']['gridId'])
-    gridX = str(parse_json['properties']['gridX'])
-    gridY = str(parse_json['properties']['gridY'])
-    add2 = gridId + '/'+gridX+','+gridY+'/forecast'
-    response_API2 = requests.get('https://api.weather.gov/gridpoints/'+add2)
-    data2 = response_API2.text
-    parse_json2 = json.loads(data2)
-    #print(parse_json2['properties'])
-    weatherdata = parse_json2['properties']['periods']
-    weathertable = pd.DataFrame.from_dict(weatherdata)
-  return weathertable[['name','temperature','temperatureUnit','windSpeed','detailedForecast']][1:2]
+    weatherdata = None
+    trynummax = 20
+    trynum=0
+    while trynum <= trynummax:
+        try:
+            add = '/'+str(lat)+','+str(lon)
+            response_API = requests.get('https://api.weather.gov/points'+add)
+            data = response_API.text
+            parse_json = json.loads(data)
+            gridId = str(parse_json['properties']['gridId'])
+            gridX = str(parse_json['properties']['gridX'])
+            gridY = str(parse_json['properties']['gridY'])
+            add2 = gridId + '/'+gridX+','+gridY+'/forecast'
+            response_API2 = requests.get('https://api.weather.gov/gridpoints/'+add2)
+            data2 = response_API2.text
+            parse_json2 = json.loads(data2)
+            weatherdata = parse_json2['properties']['periods']
+            weathertable = pd.DataFrame.from_dict(weatherdata)
+            return weathertable[['name','temperature','temperatureUnit','windSpeed','detailedForecast']][1:2]
+        except:
+            trynum +=1
+            print("API connection failed, try again...")
+            pass
+    return pd.DataFrame(columns=['name','temperature','temperatureUnit','windSpeed','detailedForecast'])
 
+# The resort_recommender function will calculate the most suitable 5 resorts based on user input.
 def resort_recommender(difficulty, goal, fav_resort, exppts, goalpts, resortpts):
-  print('User answered: ', difficulty, goal, fav_resort)
-  print(os.getcwd())
+  #print('User answered: ', difficulty, goal, fav_resort)
+  #print(os.getcwd())
   # Get the new dataframe newdf with similarity column added
   df = pd.read_csv ('data/resort.csv')
   df.loc[:,'similarity']=0
@@ -122,13 +119,11 @@ def resort_recommender(difficulty, goal, fav_resort, exppts, goalpts, resortpts)
   newdf.loc[:,'resort_rank'] = newdf['similarity'].rank()
   newdf.loc[:, 'totalpts']= newdf['exp_rank']*int(exppts) + newdf['goal_rank']*int(goalpts) + newdf['resort_rank']*int(resortpts)
   fdf = newdf.nlargest(5, 'totalpts')
-  loclist = list(zip(fdf.lat, fdf.lon))
-  #print(fdf)
-  #results = fdf.iloc[: ,:16]
+  #loclist = list(zip(fdf.lat, fdf.lon))
   results = fdf[['resort_name', 'summit','base','vertical','lifts','runs','acres','green_acres','blue_acres','black_acres','lat','lon']]
   wf = pd.DataFrame()
-  results['12hr_temperature_F']=0
-  results['12hr_forecast_details']=0
+  results['12hr_temperature (F)']=0
+  results['12hr_forecast (details)']=0
   #print(results)
   for index, row in results.iterrows():
     rlat = row['lat']
@@ -136,10 +131,10 @@ def resort_recommender(difficulty, goal, fav_resort, exppts, goalpts, resortpts)
     wf=get_weather(rlat,rlon)
     fc = wf['detailedForecast'].values[0]
     tf = wf['temperature'].values
-    results.loc[index,'12hr_temperature_F']=tf
-    results.loc[index,'12hr_forecast_details']=fc
+    results.loc[index,'12hr_temperature (F)']=tf
+    results.loc[index,'12hr_forecast (details)']=fc
   results = results.drop(columns=['lat', 'lon'])
-  results = results.rename(columns={"summit": "summit_elevation (ft)", "vertical": "vertical_drop (ft)", "base":"base_elevation (ft)","green_acres":"green_trail_acres","blue_acres":"blue_trail_acres","black_acres":"black_trail_acres"})
+  results = results.rename(columns={"summit": "summit_elevation (ft)", "vertical": "vertical_drop (ft)", "base":"base_elevation (ft)","green_acres":"green_trail (acres)","blue_acres":"blue_trail (acres)","black_acres":"black_trail (acres)"})
   results['lifts'] = results['lifts'].astype('int')
   results['runs'] = results['runs'].astype('int')
   return results
