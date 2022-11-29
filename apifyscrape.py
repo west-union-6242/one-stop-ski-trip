5e96811e571a005ea60c11bba122ad808a078159
mpanlee3 <mpanlee3@gatech.edu>
2022-11-28 23:48:06 -0800

    scrape remain data with reverse geocoding

--- apifyscrape.py
+++ apifyscrape.py
@@ -1,6 +1,9 @@
 
 from apify_client import ApifyClient
 from datetime import datetime
+import os
+import csv
+import urllib.request, json
 
 def apify_download(token, taskid, addr):
     try:
@@ -21,3 +24,27 @@ def apify_download(token, taskid, addr):
             csvfile.write(csv)
     except Exception as e:
         print("apify_download catch:", e)
+
+req = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=<token>"
+
+f = "data/resort.csv"
+with open(f, encoding = "utf8") as infile:
+    reader = csv.DictReader(infile)
+    count = 0
+    for row in reader:
+        if count >= 0:
+            print("count:", count, "resort_name:", row["resort_name"], "lat:", row["lat"], "lon:", row["lon"])
+            with urllib.request.urlopen(req.format(row["lat"], row["lon"])) as url:
+                resp = json.load(url)
+                addr_comp = resp["results"][0]["address_components"]
+                cid = -1
+                for idx in range(len(addr_comp)):
+                    if "country" in addr_comp[idx]["types"]:
+                        cid = idx
+                        break
+                if cid != -1:
+                    addr = addr_comp[cid-2]["long_name"] + ", " + addr_comp[cid-1]["long_name"] + ", " + addr_comp[cid]["long_name"]
+                    print(addr)
+                    apify_download("<token>", "<task>", addr)
+        count += 1
+
